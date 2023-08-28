4d157a66a7b827fc7f6345ae54ee323730a8d22d
Jesús Enrique Rascón <blograso@gmail.com>
2023-08-28 15:31:41 +0200

    ornaments and details

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -1,13 +1,17 @@
-<footer
-	class="caps mt-20 border-t border-black flex flex-col sm:flex-row justify-between items-start"
->
-	<div class="sm:w-1/2 balanced">
-		«El acto sedicionista por excelencia consiste en salir a la calle y disparar al azar sobre los
-		transeúntes»
+<footer class="mt-32">
+	<div class="ornament flex flex-col text-center justify-end mb-10 text-2xl">e</div>
+	<div class="caps border-t border-black justify-between items-start">
+		<div class="sm:w-1/2 balanced">
+			«El acto sedicionista por excelencia consiste en salir a la calle y disparar al azar sobre los
+			transeúntes»
+		</div>
 	</div>
 </footer>
 
 <style>
+	.ornament {
+		font-family: 'Bodoni Ornaments';
+	}
 	.caps {
 		font-variant: small-caps;
 	}
--- src/routes/participar/+page.svelte
+++ src/routes/participar/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Title from '../../lib/Components/Title.svelte';
 	import Sediciosa from '../../lib/Components/Sediciosa.svelte';
+	import { At, Envelope, EnvelopeOpen, MicrosoftWordLogo, TextAa } from 'phosphor-svelte';
 </script>
 
 <svelte:head>
@@ -9,23 +10,50 @@
 
 <Title>Cómo participar</Title>
 
-<div class="text-2xl font-medium mb-5">
+<div class="text-2xl xl:text-3xl font-medium mb-10">
 	Algunas guías relativamente relajadas acerca de cómo exponer tu trabajo en <Sediciosa />.
 </div>
 
-<ul class="text-xl list-disc">
+<ul class="text-2xl list-none flex flex-col space-y-4">
 	<li>
-		Envía dos documentos Word (uno con nombre o pseudónimo + texto y otro con texto sin nombre o
-		pseudónimo) al correo <a
+		Envía dos documentos <span class="inline-block -my-1"
+			><MicrosoftWordLogo weight="duotone" /></span
+		>
+		Word (uno con nombre o pseudónimo + texto y otro con texto sin nombre o pseudónimo) al correo
+		<span class="inline-block -my-1"><EnvelopeOpen weight="duotone" /></span>
+		<a
 			class="underline font-bold hover:text-neutral-400 transition-colors"
 			href="mailto:lasediciosa@gmail.com">lasediciosa@gmail.com</a
 		>
 	</li>
 	<li>
-		Fuente: <a
+		<span class="inline-block -my-1"><TextAa weight="fill" /></span> Fuente:
+		<a
 			class="underline hover:text-neutral-400 transition-colors"
 			href="https://fonts.google.com/specimen/EB+Garamond">EB Garamond</a
 		>, tamaño 12 puntos, interlineado 1.15.
 	</li>
-	<li>Tema, forma, estilo y género literario libres.</li>
+	<li>
+		<span class="ornaments">[</span> Tema, forma, estilo y género literario libres.
+		<span class="ornaments">\</span>
+	</li>
 </ul>
+
+<style>
+	.ornaments {
+		font-family: 'Bodoni Ornaments';
+	}
+
+	ul li::before {
+		content: '8'; /* Add content: \2022 is the CSS Code/unicode for a bullet */
+		display: inline-block; /* Needed to add space between the bullet and the text */
+
+		font-family: 'Bodoni Ornaments';
+		font-size: 0.8em;
+
+		width: 1em; /* Also needed for space (tweak if needed) */
+
+		margin-left: -1em; /* Also needed for space (tweak if needed) */
+		margin-right: 0em;
+	}
+</style>
