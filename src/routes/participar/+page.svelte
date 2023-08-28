14680458abfcbb03424b983adc22832a5f2b8861
Jesús Enrique Rascón <blograso@gmail.com>
2023-08-28 14:14:56 +0200

    yeah

--- src/routes/participar/+page.svelte
+++ src/routes/participar/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Title from '../../lib/Components/Title.svelte';
+	import Sediciosa from '../../lib/Components/Sediciosa.svelte';
 </script>
 
 <svelte:head>
@@ -8,6 +9,23 @@
 
 <Title>Cómo participar</Title>
 
-<div>pos aquí iría cómo participar</div>
+<div class="text-2xl font-medium mb-5">
+	Algunas guías relativamente relajadas acerca de cómo exponer tu trabajo en <Sediciosa />.
+</div>
 
-<div>jaja</div>
+<ul class="text-xl list-disc">
+	<li>
+		Envía dos documentos Word (uno con nombre o pseudónimo + texto y otro con texto sin nombre o
+		pseudónimo) al correo <a
+			class="underline font-bold hover:text-neutral-400 transition-colors"
+			href="mailto:lasediciosa@gmail.com">lasediciosa@gmail.com</a
+		>
+	</li>
+	<li>
+		Fuente: <a
+			class="underline hover:text-neutral-400 transition-colors"
+			href="https://fonts.google.com/specimen/EB+Garamond">EB Garamond</a
+		>, tamaño 12 puntos, interlineado 1.15.
+	</li>
+	<li>Tema, forma, estilo y género literario libres.</li>
+</ul>
