28aed181940bbb20070d123fbcb311c74535696c
Jesús Enrique Rascón <blograso@gmail.com>
2024-01-21 16:40:47 +0100

    fix change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: { fontFamily: { sans: ['EB Garamond'], ornament: ['Bodoni Ornaments'] } }
+    extend: {
+      fontFamily: { sans: ['EB Garamond'], serif: ['EB Garamond'], ornament: ['Bodoni Ornaments'] }
+    }
   },
 
   plugins: []
